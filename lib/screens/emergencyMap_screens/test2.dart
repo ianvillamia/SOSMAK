33edbb6e3e1213c5b96917b729f7ae3932d349c5
Ciaossuuu <rhowelp@gmail.com>
2021-01-18 14:29:28 +0800

    fix google map route

--- lib/screens/emergencyMap_screens/test2.dart
+++ lib/screens/emergencyMap_screens/test2.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+// Stores the Google Maps API Key
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geocoding/geocoding.dart' as coding;
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart' as gPlaces;
 
 import 'dart:math' show cos, sqrt, asin;
 
@@ -11,28 +14,23 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  CameraPosition _initialLocation =
-      CameraPosition(target: LatLng(37.4219983, -122.084));
-  // CURRENT POS: Lat: 37.4219983, Long: -122.084
-
+  CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
   GoogleMapController mapController;
-
-  final Geolocator _geolocator = Geolocator();
 
   Position _currentPosition;
   String _currentAddress;
-
+  final Geolocator _geolocator = Geolocator();
   final startAddressController = TextEditingController();
   final destinationAddressController = TextEditingController();
 
+  final startAddressFocusNode = FocusNode();
+  final desrinationAddressFocusNode = FocusNode();
+
+  gPlaces.GoogleMapsPlaces _places = gPlaces.GoogleMapsPlaces(
+      apiKey: 'AIzaSyCZjrzw-ltJyYGJqNLFLwuGzxuZSSX6ig8');
   String _startAddress = '';
   String _destinationAddress = '';
   String _placeDistance;
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
-  }
 
   Set<Marker> markers = {};
 
@@ -44,9 +42,9 @@ class _MapViewState extends State<MapView> {
 
   Widget _textField({
     TextEditingController controller,
+    FocusNode focusNode,
     String label,
     String hint,
-    String initialValue,
     double width,
     Icon prefixIcon,
     Widget suffixIcon,
@@ -54,12 +52,12 @@ class _MapViewState extends State<MapView> {
   }) {
     return Container(
       width: width * 0.8,
-      child: TextFormField(
+      child: TextField(
         onChanged: (value) {
           locationCallback(value);
         },
         controller: controller,
-        // initialValue: initialValue,
+        focusNode: focusNode,
         decoration: new InputDecoration(
           prefixIcon: prefixIcon,
           suffixIcon: suffixIcon,
@@ -191,17 +189,29 @@ class _MapViewState extends State<MapView> {
         Position _northeastCoordinates;
         Position _southwestCoordinates;
 
-        // Calculating to check that
-        // southwest coordinate <= northeast coordinate
-        if (startCoordinates.latitude <= destinationCoordinates.latitude) {
-          _southwestCoordinates = startCoordinates;
-          _northeastCoordinates = destinationCoordinates;
-        } else {
-          _southwestCoordinates = destinationCoordinates;
-          _northeastCoordinates = startCoordinates;
-        }
+        // Calculating to check that the position relative
+        // to the frame, and pan & zoom the camera accordingly.
+        double miny =
+            (startCoordinates.latitude <= destinationCoordinates.latitude)
+                ? startCoordinates.latitude
+                : destinationCoordinates.latitude;
+        double minx =
+            (startCoordinates.longitude <= destinationCoordinates.longitude)
+                ? startCoordinates.longitude
+                : destinationCoordinates.longitude;
+        double maxy =
+            (startCoordinates.latitude <= destinationCoordinates.latitude)
+                ? destinationCoordinates.latitude
+                : startCoordinates.latitude;
+        double maxx =
+            (startCoordinates.longitude <= destinationCoordinates.longitude)
+                ? destinationCoordinates.longitude
+                : startCoordinates.longitude;
 
-        // Accomodate the two locations within the
+        _southwestCoordinates = Position(latitude: miny, longitude: minx);
+        _northeastCoordinates = Position(latitude: maxy, longitude: maxx);
+
+        // Accommodate the two locations within the
         // camera view of the map
         mapController.animateCamera(
           CameraUpdate.newLatLngBounds(
@@ -291,6 +301,12 @@ class _MapViewState extends State<MapView> {
       width: 3,
     );
     polylines[id] = polyline;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
   }
 
   @override
@@ -394,7 +410,6 @@ class _MapViewState extends State<MapView> {
                           _textField(
                               label: 'Start',
                               hint: 'Choose starting point',
-                              initialValue: _currentAddress,
                               prefixIcon: Icon(Icons.looks_one),
                               suffixIcon: IconButton(
                                 icon: Icon(Icons.my_location),
@@ -404,6 +419,7 @@ class _MapViewState extends State<MapView> {
                                 },
                               ),
                               controller: startAddressController,
+                              focusNode: startAddressFocusNode,
                               width: width,
                               locationCallback: (String value) {
                                 setState(() {
@@ -414,9 +430,9 @@ class _MapViewState extends State<MapView> {
                           _textField(
                               label: 'Destination',
                               hint: 'Choose destination',
-                              initialValue: '',
                               prefixIcon: Icon(Icons.looks_two),
                               controller: destinationAddressController,
+                              focusNode: desrinationAddressFocusNode,
                               width: width,
                               locationCallback: (String value) {
                                 setState(() {
@@ -439,6 +455,8 @@ class _MapViewState extends State<MapView> {
                             onPressed: (_startAddress != '' &&
                                     _destinationAddress != '')
                                 ? () async {
+                                    startAddressFocusNode.unfocus();
+                                    desrinationAddressFocusNode.unfocus();
                                     setState(() {
                                       if (markers.isNotEmpty) markers.clear();
                                       if (polylines.isNotEmpty)
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -49,7 +49,7 @@ class _HomeState extends State<Home> {
 
   _buildTiles() {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 12),
       child: Wrap(
         children: [
           _buildTile(
